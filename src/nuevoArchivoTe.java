5303a1fe3c48abc7d4bde49501fd32b5a589e559
JohnPV894 <arenalessantiago1@gmail.com>
2025-02-13 23:55:04 +0100

    commit 2 cambio de clases

---
+++ src/nuevoArchivoTe.java
@@ -0,0 +1,30 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class nuevoArchivoTe {
+   private String nombre;
+   private int gCafe;
+   private int gLeche;
+   private int gCacaco;
+
+   public nuevoArchivoTe(String var1, int var2, int var3, int var4) {
+      this.nombre = var1;
+      this.gCafe = var2;
+      this.gLeche = var3;
+      this.gCacaco = var4;
+   }
+
+   public String getNombre() {
+      return this.nombre;
+   }
+
+   public int getgCafe() {
+      return this.gCafe;
+   }
+
+   public int getgLeche() {
+      return this.gLeche;
+   }
+
+   public int getgCacaco() {
+      return this.gCacaco;
+   }
+}
